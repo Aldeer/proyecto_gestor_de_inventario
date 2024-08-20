e86b12df0354741e621c969ab4cb89796873a085
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-20 18:23:54 -0300

    crea ventana principal de la aplicacion

---
+++ views/ventanaprincipal.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+class VentanaPrincipal(Tk):
+    """
+    Clase VentanaPrincipal que herda de la clase Tk para crear una aplicacion
+    tkinter
+    """
+
+    def __init__(self) -> None:
+        """
+        Metodo constructor de la ventana principal. Inicializa todos los widgets
+        de la aplicacion gestor de inventario.
+        """
