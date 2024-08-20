c95509cd163fb197aa3f4ad6eb9050728b589f03
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-20 18:29:10 -0300

    define la configuracion de la ventana principal

--- views/ventanaprincipal.py
+++ views/ventanaprincipal.py
@@ -11,3 +11,12 @@ class VentanaPrincipal(Tk):
         Metodo constructor de la ventana principal. Inicializa todos los widgets
         de la aplicacion gestor de inventario.
         """
+        self._configuracion()
+
+    def _configuracion(self) -> None:
+        """
+        Carga las configuraciones basicas de la ventana principal.
+        """
+        self.title("GESTOR DE INVENTARIO") # titulo de la ventana principal
+        self.geometry("1000x600") # tamaño de la ventana principal
+        self.resizable(width=False, height=False) # desactiva la redimensionado
