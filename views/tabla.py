2b9b4cedd9633f240b6e0bd78a80452818248f8b
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-22 19:26:14 -0300

    crea la cabecera de la tabla

--- views/tabla.py
+++ views/tabla.py
@@ -1,6 +1,7 @@
 from tkinter.ttk import Treeview
-from tkinter import Tk
 from tkinter import Frame
+
+CABECERA = ("CODIGO", "PRODUCTO", "PRECIO", "STOCK")
 
 class TablaProducto(Treeview):
     """
@@ -8,10 +9,23 @@ class TablaProducto(Treeview):
     Treeview de tkinter
     """
 
-    def __init__(self:Treeview, master:Tk | Frame) -> None:
+    def __init__(self, master:Frame) -> None:
         """
         Metodo constructor de clase. crea un widget tabla donde se muestran 
         los productos.
         Argumentos:
-            -master(Tk | Frame): widgets padre al que pertenece
+            -master (Frame): widgets padre al que pertenece
         """
+        super().__init__(master)
+
+        self.configure(columns=CABECERA)
+        self.cabecera()
+
+        self.grid(sticky="nsew", padx=20, pady=20)
+
+
+    def cabecera(self) -> None:
+        self.configure(show="headings")
+        for cabecera in CABECERA:
+            self.heading(cabecera, text=cabecera)
+
