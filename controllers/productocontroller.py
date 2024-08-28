e0b89b7bba81e220f513e8ab4e6b9d1375b73e0e
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-28 19:23:45 -0300

    importa las vistas para que funcione la aplicacion

--- controllers/productocontroller.py
+++ controllers/productocontroller.py
@@ -1,10 +1,30 @@
-from models.inventario import Inventario
+from models.producto import Producto
 from views.ventanaprincipal import VentanaPrincipal
+from views.formulario import Formulario
+from .formulariocontroller import FormularioController
 
 class ProductoController:
 
-    def __init__(self, inventario: Inventario, vista: VentanaPrincipal) -> None:
+    def __init__(self, inventario: Producto, vista: VentanaPrincipal) -> None:
         self.inventario = inventario
         self.vista = vista
 
-        
+        self.vista.registrar_btn.bind("<Button-1>", lambda e: self.registrar_producto(e))
+
+    def run(self) -> None:
+        """
+        Metodo del controlador que corre la aplicacion.
+        """
+        self.vista.mainloop()
+
+    def registrar_producto(self, event) -> None:
+        FormularioController(Formulario(self.vista))
+
+    def mostrar_lista_productos(self) -> None:
+        pass
+
+    def editar_producto(self) -> None:
+        pass
+
+    def eliminar_producto(self) -> None:
+        pass
