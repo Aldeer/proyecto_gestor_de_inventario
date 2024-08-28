c21d9cbc765de6b8934aa19ef6a40181f058eb11
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-28 19:24:15 -0300

    usa el controlador principal para iniciar la aplicacion

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
+from views.ventanaprincipal import VentanaPrincipal
+from models.inventario import Inventario
+from controllers.productocontroller import ProductoController
 
+ventana = VentanaPrincipal()
+modelo = Inventario()
 
+controlador = ProductoController(modelo, ventana)
+controlador.run()
