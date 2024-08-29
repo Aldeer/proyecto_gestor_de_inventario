5701de5f130b372a3da80642997c511ca465168f
Emanuel Gomila <gomilaemanuel@gmail.com>
2024-08-29 18:59:07 -0300

    configurando los argumentos que se deben pasar

--- controllers/productocontroller.py
+++ controllers/productocontroller.py
@@ -1,11 +1,11 @@
-from models.producto import Producto
+from models.inventario import Inventario
 from views.ventanaprincipal import VentanaPrincipal
 from views.formulario import Formulario
 from .formulariocontroller import FormularioController
 
 class ProductoController:
 
-    def __init__(self, inventario: Producto, vista: VentanaPrincipal) -> None:
+    def __init__(self, inventario: Inventario, vista: VentanaPrincipal) -> None:
         self.inventario = inventario
         self.vista = vista
 
@@ -18,7 +18,12 @@ class ProductoController:
         self.vista.mainloop()
 
     def registrar_producto(self, event) -> None:
-        FormularioController(Formulario(self.vista))
+        print(self.inventario.mostrar_inventario())
+        formulario_controller = FormularioController(
+            Formulario(self.vista),
+            self.inventario,
+            self.vista.tabla
+            )
 
     def mostrar_lista_productos(self) -> None:
         pass
